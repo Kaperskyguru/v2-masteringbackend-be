f2286d3f146db0edf340964061ada63c4651c45b
Solomon Eseme <kaperskyguru@gmail.com>
2023-04-19 15:07:28 +0100

    fix: database config

--- config/database.ts
+++ config/database.ts
@@ -47,7 +47,16 @@ export default ({ env }) => {
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      debug: false,
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10), acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 50000,
+        idleTimeoutMillis: 300000,
+        reapIntervalMillis: 10000,
+        createRetryIntervalMillis: 2000,
+        propagateCreateError: false
+      },
     },
     sqlite: {
       connection: {
