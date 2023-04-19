43ed9888245a04c9a9c8ec5b222029e5b67fc0fc
Solomon Eseme <kaperskyguru@gmail.com>
2023-04-19 23:04:30 +0100

    feat: set up for heroku

---
+++ config/env/production/server.js
@@ -0,0 +1,7 @@
+export default ({ env }) => ({
+    proxy: true,
+    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+    app: {
+        keys: env.array('APP_KEYS')
+    },
+});
