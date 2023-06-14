a6254b30e8c06deffda925d5816b92a8adaf84a3
Solomon Eseme <kaperskyguru@gmail.com>
2023-06-14 10:28:37 +0100

    feat: add slugify and AWS S3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,28 @@
 export default ({ env }) => ({
-    upload: {
-        config: {
-            provider: 'aws-s3',
-            providerOptions: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
-                region: env('AWS_REGION'),
-                params: {
-                    Bucket: env('AWS_BUCKET_NAME'),
-                },
-            },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET_NAME"),
         },
-    }
+      },
+    },
+  },
+
+  slugify: {
+    enabled: true,
+    config: {
+      shouldUpdateSlug: true,
+      contentTypes: {
+        post: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
 });
