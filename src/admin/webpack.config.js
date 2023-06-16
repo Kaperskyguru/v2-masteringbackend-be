7d257564de38aa1fc7e87df7516470d538012ae5
Solomon Eseme <kaperskyguru@gmail.com>
2023-06-16 19:05:46 +0100

    feat: add tip tap editor

---
+++ src/admin/webpack.config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  config.plugins.push(
+    new webpack.NormalModuleReplacementPlugin(
+      /^tippy\.js$/,
+      "tippy.js/dist/tippy-bundle.umd.min.js"
+    )
+  );
+
+  return config;
+};
